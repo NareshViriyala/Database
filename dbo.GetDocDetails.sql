7f291e6dc432cb3f035e7ba70749141dad758c20
NareshViriyala <34858361+NareshViriyala@users.noreply.github.com>
2018-01-10 20:11:31 +0530

    created more procs

---
+++ dbo.GetDocDetails.sql
@@ -0,0 +1,13 @@
+DROP PROCEDURE dbo.GetDocDetails
+GO
+CREATE PROCEDURE dbo.GetDocDetails(@Input NVARCHAR(64))
+AS
+BEGIN
+	SELECT * 
+	  FROM dbo.Doctor d (NOLOCK)	  
+	 WHERE @Input = CASE WHEN ISNUMERIC(@Input) = 1 THEN CAST(DocId AS NVARCHAR(64)) ELSE DocGUID END
+END
+
+/*
+EXEC dbo.GetDocDetails '2' 
+*/
---
+++ dbo.GetHospDetails.sql
@@ -0,0 +1,25 @@
+DROP PROCEDURE dbo.GetHospDetails
+GO
+CREATE PROCEDURE dbo.GetHospDetails(
+	   @Input NVARCHAR(64) --@Input could be DocId or DocGUID or HospID
+	 , @InputType NVARCHAR(10) --If @InputType = 'Doc' Then @Input could be DocId or DocGUID 
+							   --If @InputType = 'Hosp' Then @Input is HospId
+)
+AS
+BEGIN
+
+	IF @InputType = 'Doc'
+		SELECT h.*
+		  FROM dbo.Hospital h (NOLOCK)
+		  JOIN dbo.Doctor d (NOLOCK)
+			ON h.HospId = d.HospId
+		 WHERE @Input = CASE WHEN ISNUMERIC(@Input) = 1 THEN CAST(d.DocId AS NVARCHAR(64)) ELSE d.DocGUID END
+	ELSE
+		SELECT * 
+		  FROM dbo.Hospital (NOLOCK)
+		 WHERE HospId = @Input
+END
+
+/*
+EXEC dbo.GetHospDetails '1', 'Hosp' 
+*/
